27371824d64ba385d7bdb00b96a65c00f74e7669
Alex Baines-Buffery <alexbainesbuffery@gmail.com>
2017-06-30 12:51:10 +0100

    I am doing a commit and the point where list/test.py is down to one test

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page #2
-
+from django.template.loader import render_to_string
 '''
 class SmokeTest(TestCase):
     
@@ -12,14 +12,7 @@ class SmokeTest(TestCase):
 '''
 class HomePageTest(TestCase):
     
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/') #1
-        self.assertEqual(found.func, home_page) #1
-            # this is checking that the root of the app '/' is the home_page
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()#1
-        response = home_page(request)#2
-        html = response.content.decode('utf8')#3
-        self.assertTrue(html.startswith('<html>'))#4
-        self.assertIn('<title>To-Do lists</title>', html)#5
-        self.assertTrue(html.endswith('</html>'))#4
+    def test_uses_home_template(self):
+
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
