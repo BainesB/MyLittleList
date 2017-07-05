9f0860464daa5f25d98c697f88fcf73174211548
Alex Baines-Buffery <alexbainesbuffery@gmail.com>
2017-07-05 17:48:15 +0100

    Model for list Items and associated migration. Missed .text off the end of a test. had to debug

--- lists/tests.py
+++ lists/tests.py
@@ -4,12 +4,15 @@ from django.http import HttpRequest
 
 from lists.views import home_page #2
 from django.template.loader import render_to_string
+from django.db import models
+from lists.models import Item
 '''
 class SmokeTest(TestCase):
     
     def test_bad_maths(self):
             self.assertEqual(1 + 1, 3)
 '''
+
 class HomePageTest(TestCase):
     
     def test_uses_home_template(self):
@@ -21,3 +24,21 @@ class HomePageTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
+class ItemModelTest(TestCase):
+    
+    def test_saving_andretrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The fist (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_item = Item.objects.all()
+        self.assertEqual(saved_item.count(),2)
+
+        first_saved_item = saved_item[0]
+        second_saved_item = saved_item[1]
+        self.assertEqual(first_saved_item.text, 'The fist (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
