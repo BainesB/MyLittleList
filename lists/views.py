1cebd9f04bee59bba178e6d56ed4bd29f777eb6e
Alex Baines-Buffery <alexbainesbuffery@gmail.com>
2017-07-28 12:48:33 +0100

    na

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 from django.http import HttpResponse 
 from django.shortcuts import render
@@ -7,11 +7,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items':items}) 
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
