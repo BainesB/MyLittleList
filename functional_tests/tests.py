62accb0065716c81a1afaa44385efb31b2e8fb4b
Alex Baines-Buffery <alexbainesbuffery@gmail.com>
2017-08-03 10:46:00 +0100

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -83,7 +83,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk') 
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('2: Buy milk') 
+        self.wait_for_row_in_list_table('1: Buy milk') # You have changed this.  from 2 / 1. 
 
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
